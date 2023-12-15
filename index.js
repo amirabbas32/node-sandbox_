8cff057b9731a04e9caac553928cb04d8f2e144f
CodeSandbox <support@codesandbox.io>
2023-12-15 20:41:25 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello CodeSandbox");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  console.log(__dirname);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
